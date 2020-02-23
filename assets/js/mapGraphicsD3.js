013f0fcbcaad68179feb540cea2aec18568bb860
MauricioMeza <mmezab@unal.edu.co>
2020-02-23 02:20:31 -0500

    Added total emigrants chart

---
+++ assets/js/mapGraphicsD3.js
@@ -0,0 +1,121 @@
+//BARCHAR
+var rawData1 = `pais,v2018
+Colombia,1630000
+Peru,861000
+Ecuador,385000
+Chile,371000
+USA,340000
+España,320000
+Brasil,253000
+Argentina,182000`;
+
+var rawData2 = `y,cantidad
+2010,600000
+2011,700000
+2012,900000
+2013,1000000
+2014,1500000
+2015,1800000
+2016,2530000
+2017,3000000
+2018,4000000`;
+
+function drawBarGraph(rawData){
+    var svg = d3.select("#barChar")
+    var margin = ({top: 20, right: 0, bottom: 0, left: 60});
+    var data = d3.csvParse(rawData, ({pais, v2018}) => ({name: pais, value2018: v2018 })).sort((a, b) => b.value2018 - a.value2018);
+    var height = data.length * 25 + margin.top + margin.bottom;
+    var width = svg.style("width").replace("px", "");
+
+    var x = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.value2018/(width/375))])
+    .range([margin.left, width - margin.right]);
+
+    var y = d3.scaleBand()
+    .domain(d3.range(data.length))
+    .range([margin.top, height - margin.bottom])
+    .padding(0.1);
+
+    var xAxis = g => g
+        .attr("transform", `translate(0,${margin.top})`)
+        .call(d3.axisTop(x).ticks(width / 80))
+        .call(g => g.select(".domain").remove())
+
+    var yAxis = g => g
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y).tickFormat(i => data[i].name).tickSizeOuter(0))
+
+    var format = x.tickFormat(20);
+
+    svg.append("g")
+        .attr("fill", "steelblue")
+    .selectAll("rect")
+    .data(data)
+    .join("rect")
+        .attr("x", x(0))
+        .attr("y", (d, i) => y(i))
+        .attr("width", d => (x(d.value2018) - x(0)))
+        .attr("height", y.bandwidth());
+
+    svg.append("g")
+        .attr("fill", "black")
+        .attr("text-anchor", "end")
+        .style("font", "12px sans-serif")
+    .selectAll("text")
+    .data(data)
+    .join("text")
+        .attr("x", d => x(d.value2018) + 52)
+        .attr("y", (d, i) => y(i) + y.bandwidth() / 2)
+        .attr("dy", "0.35em")
+        .text(d => format(d.value2018));
+        
+    svg.append("g").call(xAxis);
+    svg.append("g").call(yAxis);
+}
+
+//LINECHAR
+function drawLineGraph(rawData){
+	var data = d3.csvParse(rawData, ({y, cantidad}) => ({year: y, value: cantidad})).sort((a, b) => b.year - a.year)
+    var svg = d3.select("#lineChar")
+	var height = svg.style("height").replace("px", "")
+	var width = svg.style("width").replace("px", "");
+	
+    var margin = ({top: 10, right: 13, bottom: 20, left: 60})
+
+    var x = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.year))
+        .range([margin.left, width - margin.right])
+    var y = d3.scaleLinear()
+        .domain([0, 4000000])
+        .range([height - margin.bottom, margin.top])
+    
+    var xAxis = g => g
+        .attr("transform", `translate(0,${height - margin.bottom})`)
+        .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(10))
+		
+    var yAxis = g => g
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y))
+        .call(g => g.select(".domain").remove())
+    
+    var curve = d3.curveLinear
+    var area = d3.area()
+        .curve(curve)
+        .x(d => x(d.year))
+        .y0(y(0))
+        .y1(d => y(d.value))
+
+    svg.append("path")
+        .datum(data)
+        .attr("fill", "steelblue")
+        .attr("d", area);
+
+    svg.append("g")
+        .call(xAxis);
+
+    svg.append("g")
+        .call(yAxis);
+}
+
+drawBarGraph(rawData1);
+drawLineGraph(rawData2)
--- assets/js/mapInteraction.js
+++ assets/js/mapInteraction.js
@@ -1,33 +1,57 @@
 function addText(year){
     var text
+    var img
     switch(year){
         case 2010:
             text = "<b>2010:</b> Hugo chavez lleva 8 años en el gobierno de la Revolucion Bolivariana. Algunos venezolanos opuestos a esta habian salido del pais durante la anterior decada principalmente a destinos como Europa y EEUU. A finales de año su partido pierde la mayoria en el senado y la oposicion al Chavez comienza a tomar fuerza."
+            img = "assets/img/img2010.png";
             break;
         case 2011:
             text = "<b>2011:</b> Se diagnostica a Chavez con cancer mientras una crisis energetica causada por el cambio climatico aumenta la oposicion al regimen, sin embargo un alza en el precio del petroleo le permite a Chavez mantener la economia del pais y su Revolucion Bolivariana en buena marcha manteniendo su popularidad entre sus seguidores."
+            img = "assets/img/img2011.png";
             break;
         case 2012:
             text = "<b>2012:</b> Chavez es reelegio por 3ra vez consecutiva, sin embargo su estado de salud continua empeorando delegando cada vez mas el poder a su vicepresidente Nicolas Maduro. Asi mismo el chavismo gana las eleccione regionales y su popularidad aumenta con un precio de petroleo constantemente alto y una economia en marcha."
+            img = "assets/img/img2012.png";
             break;
         case 2013:
             text = "<b>2013:</b> Despues de 13 años como presidente Hugo Chavez muere a causa de su enfermadad y la presidencia es asumida por Nicolas Maduro, aunque continua las politicas de Chavez se genera una gran incertidumbre para el futuro de Venezuela. Comienzan las protestas en su oposicion con un saldo de 9 muertos en ese año."
+            img = "assets/img/img2013.png";
             break;
         case 2014:
             text = "<b>2014:</b> Las protestas al gobierno de maduro continuan dejando casi 41 muertos y 180 heridos, a finales de año el precio del petroleo tiene una estrepitoza caida de 100 a menos de 50 dolares el barril lo cual aumenta la incertidumbre al futuro de un pais que depende de la exportacion de crudo para mantener su economia."
+            img = "assets/img/img2014.png";
             break;
         case 2015:
             text = "<b>2015:</b> Las consecuencias de la caida del petroleo no se demoran llegar aumentando el desempleo y la inflacion en el pais en el pais. Continuan las protestas contra el gobierno de Maduro mientras la oposicion logra conseguir una victoria aplastante al partido de Maduro ganando la mayoria en la asamblea nacional."
+            img = "assets/img/img2015.png";
             break;
         case 2016:
             text = "<b>2016:</b> La represion a las protestas continua generando mas victimas mientras el desempleo, la inflacion y el crimen aumentan. Maduro no permite quela nueva samblea en su oposicion se pocesione y desconoce su autoridad, esto aumenta las protestas mientras EEUU toma esto para imponer fuertes sanciones economicas al gobierno. "
+            img = "assets/img/img2016.png";
             break;
         case 2017:
             text = "<b>2017:</b> Maduro por un referendo que muchas autoridades internacionales reconocen como fraudulento llama a una Asamblea Nacional Constituyente para reemplazar el congreso en su contra. Esto dispara las protestas, sanciones, inflacion y crimen a niveles criticos que amenazan con acabar el pais."
+            img = "assets/img/img2017.png";
             break;
         case 2018:
             text = "<b>2018:</b> Este es el año con la mayor migracion de Venezolanos fuera del Pais, la mayoria saliendo del pais sin rumbo fijo en busca de empleo, estabilidad y alejarse de la crisis que enfrenta su pais, casi todos salen por la frontera con Colombia generando la crisis de refugiados mas grande en la historia de Latinoamerica."
+            img = "assets/img/img2018.png";
             break;
     }
-    document.getElementById("paragraphYear").innerHTML = text;
+    var parrafo = $("#paragraphYear");
+    var imagen = $("#imgYear");
+
+    parrafo.fadeOut(function(){
+        parrafo.html(text)
+        parrafo.fadeIn()
+    });
+
+    imagen.fadeOut(function(){
+        imagen.attr("src", img)
+        imagen.attr("height", "100px")
+        imagen.attr("width","100px")
+        imagen.fadeIn()
+    });
+    
 }
