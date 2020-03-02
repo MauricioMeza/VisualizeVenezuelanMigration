e2fd2f5e303477e601ce2651e6f4eb7277e4f95d
Gonzalo Diaz <egonzalodm@gmail.com>
2020-03-02 18:37:03 -0500

    Scripts update

--- Scripts_Datos/script_one.js
+++ Scripts_Datos/script_one.js
@@ -1,3 +1,9 @@
+//Obtener datos por pais++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function get_data_by_country(){
+    get_country($("#tx1").val(), "p1")
+}
+
 //Obtener objeto pais
 function get_country(country_name, id){
     //Cargar datos del csv
@@ -58,10 +64,68 @@ class Country {
     }
 }
 
-//get_country("Albania")
 
-function get_data(){
-    get_country($("#tx1").val(), "p1")
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Obtener datos por año******************************************************************
+function get_data_by_year(){
+    get_year($("#tx2").val(), "p2")
 }
 
+//Obtener objeto año
+function get_year(year, id){
+    //Cargar datos del csv
+    d3.csv("data.csv", conversor, function(data){
+        year_obj = select_year(data, year);
+        $("#"+id).html(year_obj.toString())
+    });
+
+}
+
+//Seleccionar los datos de todos los paises del año solicitado
+function select_year(data, year){
+    var year_obj = new Year(year);
+    var current_country = data[0]["Country / territory of asylum/residence"];
+    year_obj.countries.push([current_country,0]);
+
+    for (var i = 0; i < data.length; i ++){
+        if (current_country == data[i]["Country / territory of asylum/residence"] && year == data[i]["Year"] && !isNaN(data[i]["Value"])){
+            //Si es el mismo pais y el año es correcto, sumar el valor
+            var last = year_obj.countries.length - 1;
+            year_obj.countries[last][1] = year_obj.countries[last][1] + data[i]["Value"];
+        }else if(current_country != data[i]["Country / territory of asylum/residence"] && year == data[i]["Year"] && !isNaN(data[i]["Value"])){
+            //Si el pais es diferente, pero el año es correcto, agregar el pais con el valor
+            current_country = data[i]["Country / territory of asylum/residence"];    
+            year_obj.countries.push([current_country, data[i]["Value"]])
+        }else if(current_country != data[i]["Country / territory of asylum/residence"] && year == data[i]["Year"] && isNaN(data[i]["Value"])){
+            //Si el pais es diferente y el año es correcto, pero el valor no es un numero, agregar un cero
+            current_country = data[i]["Country / territory of asylum/residence"];    
+            year_obj.countries.push([current_country,0])
+        }else if(current_country != data[i]["Country / territory of asylum/residence"] && year != data[i]["Year"]){
+            //Si el pais es diferente, pero el año no es correcto, agregarlo con valor cero
+            current_country = data[i]["Country / territory of asylum/residence"];    
+            year_obj.countries.push([current_country,0])
+        }//Si el pais es el mismo pero el año es incorrecto, no hacer nada
+    }
+    console.log(year_obj);
+    return year_obj;
+}
+
+class Year{
+
+    constructor(year){
+        this.year = year;
+        this.countries = [];
+    }
+
+    toString(){
+        var values2 = "";
+        for(var i = 0; i < this.countries.length; i ++){
+            values2 = values2.concat("<br>"+this.countries[i][0]+" : "+this.countries[i][1]);  
+        }
+        return this.year + values2;
+    }
+}
+//***************************************************************************************
 
